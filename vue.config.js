9bf9f75abe3560eb92bd0f3794e7609ad3eb82f6
Filip Vanden Eynde <filipve1994@gmail.com>
2021-03-18 10:22:21 +0100

    remove service-worker

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,14 @@
 // https://stackoverflow.com/questions/53589853/vue-cli-3-hot-reload-suddenly-not-working-in-browsers
 module.exports = {
-  devServer: {
-    host: "localhost",
-    overlay: {
-      warnings: false,
-      errors: false
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/vue-personal-resume/'
+        : '/',
+    devServer: {
+        host: "localhost",
+        overlay: {
+            warnings: false,
+            errors: false
+        },
     },
-  },
-  lintOnSave: process.env.NODE_ENV !== 'production'
+    lintOnSave: process.env.NODE_ENV !== 'production'
 };
